53a914a1a9a9b92fa5dbc8844f2d7b92a7c5ee29
Johnson Yuen <johnson@sheepe.com>
2015-09-08 15:10:47 +1000

    Added routes and stubs for static pages index and about

---
+++ resources/views/master.blade.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Heart Smart</title>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</head>
+<body>
+<h1>This is the header</h1>
+
+<div class="container">
+    @yield('Content')
+</div>
+
+<div class="footer">
+    @yield('Footer')
+</div>
+</body>
+</html>
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,9 @@
+
+
+@extends('master')
+
+@section('Content')
+<h2>This is the index page</h2>
+@endsection
+
+@stop
