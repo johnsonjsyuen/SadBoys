413fd63792391704fe5041a6e021bb447185ddda
Johnson Yuen <johnson@sheepe.com>
2015-09-15 11:24:26 +1000

    commit bug

---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,51 @@
+
+
+@extends('master')
+
+@section('Content')
+    <div id="blockCVD" class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <h1>Contact</h1>
+                <hr>
+                {!! Form::open(['url' => 'contact']) !!}
+
+                        <!--Title-->
+                <div class="form-group">
+                    {!! Form::label ('contact', 'Contact:') !!}
+                    {!! Form::text ('contact', null, ['class' => 'form-control']) !!}
+                </div>
+                <!--Body-->
+                <div class="form-group">
+                    {!! Form::label ('email', 'Email Address:') !!}
+                    {!! Form::text ('email', null, ['class' => 'form-control']) !!}
+                </div>
+                <!--Body-->
+                <div class="form-group">
+                    {!! Form::label ('telephone', 'Telephone Number:') !!}
+                    {!! Form::text ('telephone', null, ['class' => 'form-control']) !!}
+                </div>
+
+                <div class="form-group">
+                    {!! Form::label ('message', 'Message:') !!}
+                    {!! Form::textarea ('message', null, ['class' => 'form-control']) !!}
+                </div>
+
+                <!--submit button-->
+                <div class="form-group">
+                    {!! Form::submit ('Submit Form', ['class' => 'btn btn-primary form-control']) !!}
+                </div>
+
+                {!! Form::close([ 'URL' => 'recipes']) !!}
+            </div>
+        </div>
+    </div>
+
+@endsection
+
+@stop
